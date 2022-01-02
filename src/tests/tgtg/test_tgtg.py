53b021f6d8f22d0f60fee6abd7a6c7aa6654a09d
Henning Merklinger <henning.merklinger@gmail.com>
2022-01-02 18:24:55 +0100

    update for config, random sleep time

--- src/tests/tgtg/test_tgtg.py
+++ src/tests/tgtg/test_tgtg.py
@@ -7,17 +7,22 @@ class TGTG_API_Test(unittest.TestCase):
     def test_get_items(self):
         client = TgtgClient(
             email=environ.get("TGTG_USERNAME", None),
+            timeout=60,
             access_token=environ.get("TGTG_ACCESS_TOKEN", None),
             refresh_token=environ.get("TGTG_REFRESH_TOKEN", None),
             user_id=environ.get("TGTG_USER_ID", None),
         )
+
+        ## get credentials and safe tokens to GITHUB_ENV file
+        ## this enables github workflow to reuse the access_token on sheduled runs
         credentials = client.get_credentials()
         env_file = environ.get('GITHUB_ENV', None)
         if env_file:
-            with open(env_file, "a") as myfile:
-                myfile.write("TGTG_ACCESS_TOKEN={}\n".format(credentials["access_token"]))
-                myfile.write("TGTG_REFRESH_TOKEN={}\n".format(credentials["refresh_token"]))
-                myfile.write("TGTG_USER_ID={}\n".format(credentials["user_id"]))
+            with open(env_file, "a") as file:
+                file.write("TGTG_ACCESS_TOKEN=::add-mask::{}\n".format(credentials["access_token"]))
+                file.write("TGTG_REFRESH_TOKEN=::add-mask::{}\n".format(credentials["refresh_token"]))
+                file.write("TGTG_USER_ID=::add-mask::{}\n".format(credentials["user_id"]))
+        
         data = client.get_items(favorites_only=True)
         assert len(data) > 0
         for property in GLOBAL_PROPERTIES:
