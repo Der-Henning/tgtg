391c983b6e080f1715fba676a7c898df3005026b
Henning Merklinger <henning.merklinger@gmail.com>
2023-01-25 20:08:12 +0100

    remove datadome cookie after 3. retry

--- src/tgtg/tgtg_client.py
+++ src/tgtg/tgtg_client.py
@@ -178,23 +178,26 @@ class TgtgClient:
         except ValueError:
             # Status Code == 403 and no json contend
             # --> Blocked due to rate limit / wrong user_agent.
-            # 1. Get latest APK Version from google
-            # 2. Reset current session
-            # 3. Sleep 10 seconds, after 10 errors sleep 1 hour
-            # 4. Rety request
+            # 1. Try: Get latest APK Version from google
+            # 2. Try: Reset session
+            # 3. Try: Delete datadome cookie and reset session
+            # 10.Try: Sleep 10 minutes, and reset session
             if response.status_code == 403:
                 log.debug("Captcha Error 403!")
+                self.captcha_error_count += 1
                 if self.captcha_error_count == 1:
                     self.user_agent = self._get_user_agent()
+                elif self.captcha_error_count == 2:
                     self.session = self._create_session()
-                self.captcha_error_count += 1
-                if self.captcha_error_count > 10:
+                elif self.captcha_error_count == 4:
+                    self.datadome_cookie = None
+                    self.session = self._create_session()
+                elif self.captcha_error_count >= 10:
                     log.warning(
                         "Too many captcha Errors! Sleeping for 10 minutes...")
                     time.sleep(10 * 60)
                     log.info("Retrying ...")
                     self.captcha_error_count = 0
-                    self.user_agent = self._get_user_agent()
                     self.session = self._create_session()
                 time.sleep(1)
                 return self._post(path, **kwargs)
