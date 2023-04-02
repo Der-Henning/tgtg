2f4834abbb0f4615714bf0245f3519d054b68974
Henning Merklinger <henning.merklinger@gmail.com>
2023-04-02 10:43:21 +0200

    add exception handling for order creation

--- src/models/reservations.py
+++ src/models/reservations.py
@@ -1,8 +1,11 @@
+import logging
 from dataclasses import dataclass
 from typing import Callable, Dict, List
 
 from models.item import Item
 from tgtg import TgtgClient
+
+log = logging.getLogger("tgtg")
 
 
 @dataclass
@@ -33,14 +36,18 @@ class Reservations():
             Reservation(item_id, amount, display_name))
 
     def _create_order(self, reservation: Reservation) -> None:
-        res = self.client.create_order(reservation.item_id, reservation.amount)
-        order_id = res.get("id")
-        order = Order(order_id,
-                      reservation.item_id,
-                      reservation.amount,
-                      reservation.display_name)
-        self.active_orders.append(order)
-        self.reservation_query.remove(reservation)
+        try:
+            res = self.client.create_order(
+                reservation.item_id, reservation.amount)
+            order_id = res.get("id")
+            order = Order(order_id,
+                          reservation.item_id,
+                          reservation.amount,
+                          reservation.display_name)
+            self.active_orders.append(order)
+            self.reservation_query.remove(reservation)
+        except Exception as exc:
+            log.error("Create Order Error: %s", exc)
 
     def make_orders(self, state: Dict[str, Item],
                     callback: Callable[[Reservation], None]) -> None:
