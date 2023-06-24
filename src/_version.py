8b6ea89f04bf994e9c19cd51af066fba16cedbfe
Henning Merklinger <henning.merklinger@gmail.com>
2023-06-24 07:45:25 +0200

    bump version 1.17.1

--- src/_version.py
+++ src/_version.py
@@ -1,6 +1,6 @@
 __title__ = "TGTG Scanner"
 __description__ = "Provides notifications for TGTG magic bags"
-__version__ = "1.17.0"
+__version__ = "1.17.1"
 __author__ = "Henning Merklinger"
 __author_email__ = "henning.merklinger@gmail.com"
 __license__ = "GPL"
