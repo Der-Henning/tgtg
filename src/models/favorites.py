828ff80946e53936123d0d434af38f85ceaa4f73
floriegl <floriegl@live.at>
2023-07-27 15:47:55 +0200

    Added favorite management using Telegram (#386)

---
+++ src/models/favorites.py
@@ -0,0 +1,79 @@
+import logging
+from dataclasses import dataclass
+from typing import List
+
+from models.errors import TgtgAPIError
+from models.item import Item
+from tgtg import TgtgClient
+
+log = logging.getLogger("tgtg")
+
+
+@dataclass
+class AddFavoriteRequest():
+    item_id: str
+    item_display_name: str
+    proceed: bool
+
+
+@dataclass
+class RemoveFavoriteRequest():
+    item_id: str
+    item_display_name: str
+    proceed: bool
+
+
+class Favorites():
+    def __init__(self, client: TgtgClient) -> None:
+        self.client = client
+
+    def is_item_favorite(self, item_id: str) -> bool:
+        """Returns true if the provided item ID is in the favorites
+
+        Args:
+            item_id (str): Item ID
+        Returns:
+            bool: true, if the provided item ID is in the favorites
+        """
+        return any(item for item
+                   in self.client.get_favorites()
+                   if Item(item).item_id == item_id)
+
+    def get_item_by_id(self, item_id: str) -> Item:
+        """Gets an item by the Item ID
+
+        Args:
+            item_id (str): Item ID
+        Returns:
+            Item: the Item for the Item ID or an empty Item
+        """
+        try:
+            return Item(self.client.get_item(item_id))
+        except TgtgAPIError:
+            return Item({})
+
+    def get_favorites(self) -> List[Item]:
+        """Get all favorite items
+
+        Return:
+            List: List of favorite items
+        """
+        return [Item(item) for item in self.client.get_favorites()]
+
+    def add_favorites(self, item_ids: List[str]) -> None:
+        """Adds all the provided item IDs to the favorites
+
+        Args:
+            item_ids (str): Item ID list
+        """
+        for item_id in item_ids:
+            self.client.set_favorite(item_id, True)
+
+    def remove_favorite(self, item_ids: List[str]) -> None:
+        """Removes all the provided item IDs from the favorites
+
+        Args:
+            item_ids (str): Item ID list
+        """
+        for item_id in item_ids:
+            self.client.set_favorite(item_id, False)
---
+++ tests/test_favorites.py
@@ -0,0 +1,63 @@
+from unittest.mock import MagicMock, call
+
+import pytest
+
+from models.errors import TgtgAPIError
+from models.favorites import Favorites
+
+
+@pytest.fixture
+def favorites():
+    mock_client = MagicMock()
+    return Favorites(mock_client)
+
+
+def test_is_item_favorite(favorites: Favorites, tgtg_item: dict):
+    favorites.client.get_favorites.return_value = []
+    is_favorite = favorites.is_item_favorite(
+        tgtg_item.get("item", {}).get("item_id")
+    )
+    assert is_favorite == tgtg_item.get("favorite")
+
+    is_favorite = favorites.is_item_favorite("123")
+    assert is_favorite is False
+
+
+def test_get_item_by_id(favorites: Favorites, tgtg_item: dict):
+    item_id = tgtg_item.get("item", {}).get("item_id")
+    favorites.client.get_item.side_effect = lambda x: tgtg_item \
+        if x == item_id else (_ for _ in ()).throw(TgtgAPIError())
+    item = favorites.get_item_by_id(item_id)
+    assert item.item_id == item_id
+    assert item.display_name == tgtg_item.get("display_name")
+    assert item.items_available == tgtg_item.get("items_available")
+
+    item = favorites.get_item_by_id("123")
+    assert not item.item_id
+    assert item.display_name == "-"
+    assert item.items_available == 0
+
+
+def test_get_favorites(favorites: Favorites, tgtg_item: dict):
+    favorites.client.get_favorites.return_value = [tgtg_item]
+    favorites = favorites.get_favorites()
+    assert len(favorites) == 1
+    assert favorites[0].item_id == tgtg_item.get("item", {}).get("item_id")
+    assert favorites[0].display_name == tgtg_item.get("display_name")
+    assert favorites[0].items_available == tgtg_item.get("items_available")
+
+
+def test_add_favorites(favorites: Favorites):
+    set_favorite_mock = MagicMock()
+    favorites.client.set_favorite = set_favorite_mock
+    favorites.add_favorites(["123", "234"])
+    set_favorite_mock.assert_has_calls([call("123", True), call("234", True)])
+
+
+def test_remove_favorites(favorites: Favorites):
+    set_favorite_mock = MagicMock()
+    favorites.client.set_favorite = set_favorite_mock
+    favorites.remove_favorite(["123", "234"])
+    set_favorite_mock.assert_has_calls(
+        [call("123", False), call("234", False)]
+    )
